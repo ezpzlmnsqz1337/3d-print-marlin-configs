2b1ad1fccb7aad0f60b156e4b00a93f36dd2d325
ezpzlmnsqz1337 <mazlixek@seznam.cz>
2026-01-25 22:52:56 +0100

    feat: add missing Z probe offset

--- Anet AM8 silver/Configuration.h
+++ Anet AM8 silver/Configuration.h
@@ -784,6 +784,8 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -32  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -2  // Y offset: -front +behind [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.3   // Z offset: -below +above  [the nozzle]
+
 #define MIN_PROBE_EDGE 10
 
 // X and Y axis travel speed (mm/m) between probes
