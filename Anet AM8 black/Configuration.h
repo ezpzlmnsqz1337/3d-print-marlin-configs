c10f38ab7958fb3d7e9873726f24780475042dff
ezpzlmnsqz1337 <zden.braun@seznam.cz>
2026-01-27 22:23:05 +0100

    fix: update probe offset and bed dimensions for black printer

--- Anet AM8 black/Configuration.h
+++ Anet AM8 black/Configuration.h
@@ -787,7 +787,7 @@
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 10
+#define MIN_PROBE_EDGE 57
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 6000
@@ -885,11 +885,11 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 209
+#define X_BED_SIZE 250
+#define Y_BED_SIZE 232
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -26
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
